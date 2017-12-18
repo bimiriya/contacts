3a37f7f2a8e736630247c30b871b0ef8c48fb31e
bimiriya <poyarzunaspee@gmail.com>
2017-12-18 00:30:46 -0300

    funcionalidad hecha

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,7 @@
 $(document).ready(function() {
+
     $("nav .btn-success").click(function() {
         $("#modal-add").show()
-
-        $(".add-btn").click(function() {
-            $("input").each(function() {
-                if ($(this).val().length == 0) {
-                    $(".modals button").attr("disable",true);
-                } else {
-                    $(".modals").hide();
-                }
-            })
-            $("#filler").append("<div></div>")
-
-        })
-    })
-    
-    $(".modals .glyphicon-remove").click(function() {
-        $(".modals").hide();
     })
 
     document.getElementById("num").oninput = function () {
@@ -24,5 +9,61 @@ $(document).ready(function() {
             this.value = this.value.slice(0,10); 
         }
     }
+
+    $(".add-btn").click(function() {
+        $("input").each(function() {
+            if ($(this).val().length == 0) {
+                $(".modals button").attr("disable",true);
+            } else {
+                $(".modals").hide();
+            }
+        })
+
+
+        
+        $("#filler").prepend("<li><div><button class='btn btn-default glyphicon glyphicon-trash'></button></div><h1>" + $(".name").val() + 
+        "</h1><h2>" + $(".num").val() + 
+        "</h2><span>" + $(".email").val() + 
+        "</span></li>");
+
+        $("input").val("");
+
+        $("#filler .glyphicon-trash").click(function() {
+            var parents = $(this).parent();
+            parents.remove();
+        })
+
+    })
+    
+    $(".modals .glyphicon-remove").click(function() {
+        $(".modals").hide();
+    })
+
+    $(".btn-info").click(function() {
+        $(this).hide();
+        $("#search").append("<div class='input-group'><span class='input-group-addon glyphicon glyphicon-search' id='sizing-addon1'></span><input type='text' class='form-control' placeholder='Search' aria-describedby='sizing-addon1'></div>");
+
+        function listFilter(list) {
+            
+       $("#search input").change( function () {
+           var value = $(this).val();
+           if(value) {
+             $(list).find("h1:not(:Contains(" + value + "))").parent().slideUp();
+             $(list).find("h1:Contains(" + value + ")").parent().slideDown();
+           } else {
+             $(list).find("li").slideDown();
+           }
+           return false;
+         })
+       .keyup( function () {
+           $(this).change();
+       });
+     }
+   
+     $(function () {
+       listFilter($("#filler"));
+     });
+    })
+
 
 })
